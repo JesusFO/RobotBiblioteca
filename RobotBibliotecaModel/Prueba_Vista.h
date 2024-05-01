d0ff1757b89bef4f1416118cd6e206d21b943290
José Vela <167365707+jfvela6@users.noreply.github.com>
2024-05-01 00:53:43 -0500

    D

--- RobotBibliotecaModel/Prueba_Vista.h
+++ RobotBibliotecaModel/Prueba_Vista.h
@@ -1,5 +1,12 @@
 #pragma once
 //
 //Esta es una prueba de visualizacion
+<<<<<<< HEAD
 // CAMBIO 555
 //Yp /FFFFFFFFFFF
+=======
+
+//Yp
+
+//Cambiando de nuevo //D
+>>>>>>> d1bfeb74cffa6fd0a9d5344bd5553632167e07f9
