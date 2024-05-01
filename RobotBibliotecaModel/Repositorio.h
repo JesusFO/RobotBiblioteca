7920274190def6a352b25892379be5060eb2f6d6
DaniHuaypuna <huaypunadani98@gmail.com>
2024-05-01 14:50:25 -0500

    Encabezado Repositorio

--- RobotBibliotecaModel/Repositorio.h
+++ RobotBibliotecaModel/Repositorio.h
@@ -1,23 +1,26 @@
 /**
- * Project Untitled
+ * Project Robot Biblioteca
  */
-
+#pragma once
 
 #ifndef _REPOSITORIO_H
 #define _REPOSITORIO_H
 
-class Repositorio {
-public: 
-    
-void Repositorio();
-private: 
-    int idRepositorio;
-    String Estante;
-    int Piso;
-    String Categoria;
-    String Autores;
-    String IDRepositorio;
-    String etiqueta;
-};
+using namespace System;
 
+namespace RobotBibliotecaModel {
+    public ref class Repositorio {
+    private:
+        property int idRepositorio;
+        property String^ Estante;
+        property int Piso;
+        property String^ Categoria;
+        property String^ Autores;
+        property String^ IDRepositorio;
+        property String^ etiqueta;
+    public:
+        Repositorio() {}
+        Repositorio(int idRepositorio, String^ Estante, int Piso, String^ Categoria, String^ Autores, String^ IDRepositorio, String^ etiqueta) {}
+    };
+}
 #endif //_REPOSITORIO_H
