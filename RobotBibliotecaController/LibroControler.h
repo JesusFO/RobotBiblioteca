e1904fbf5e0d299f5d506c3d5bc3c48bba26ceaf
DaniHuaypuna <huaypunadani98@gmail.com>
2024-05-02 14:37:08 -0500

    correccion
    declaración de buscarLibrosAll en el .h

--- RobotBibliotecaController/LibroControler.h
+++ RobotBibliotecaController/LibroControler.h
@@ -13,5 +13,6 @@ namespace RobotBibliotecaControler {
 			List<Libro^>^ buscarLibrosxTitulo(String^ Titulo);
 			void agregarNuevoLibro(int idlibro, String^ titulo, String^ categoria, String^ estado, int edicion, String^ reseña, String^ etiqueta, String^ autorLibro, String^ disponibilidad);
 			void escribirArchivo(List<Libro^>^ listaLibros);
+			List<Libro^>^ RobotBibliotecaControler::LibroControler::buscarLibrosAll();
 	};
 }
